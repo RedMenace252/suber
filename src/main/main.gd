65a30af559277b1973f2dac21866d5cf8fcba941
RedMenace252 <jedspooner@hotmail.co.uk>
2025-06-12 19:42:47 +0100

    scene switching SHOULD be finally fixed

--- src/main/main.gd
+++ src/main/main.gd
@@ -3,37 +3,24 @@ extends Node
 @onready var main_2d = $Scene2D
 @onready var main_3d = $Scene3D
 @onready var player2d = $Scene2D/Player2D
-@onready var player3d = $Scene3D/Player3D
-@onready var fadeinout  = $UI/FadeInOut/FadeInOutPlayer
-@onready var periscopeinout = $Scene3D/Player3D/PlayerAnimator
+var player3dtemplate = preload("res://src/player_3d/player_3d.tscn")
+var player3d
+var player3danimator
+@onready var fadecontroller = $UI/FadeInOut/FadeController
 
 var in_3d_mode = false
 
 func _ready():
-	player3d.mouse_look_enabled = false
-	player3d.set_process(false)
-	player3d.set_physics_process(false)
-	player3d.set_process_input(false)
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	fadeinout.play("fade_in")
+	fadecontroller.play("fade_in")
 	#SignalBus.emit_signal("display_dialogue", "intro")
-		
-func freeze_all_inputs():
-	set_process_input(false)
-	player2d.set_process_input(false)
-	player3d.set_process_input(false)
-	
-func unfreeze_all_inputs():
-	set_process_input(true)
-	player2d.set_process_input(true)
-	player3d.set_process_input(true)
-	
+
+
 func _input(event):
 	if event.is_action_pressed("toggle_cockpit"):
 		toggle_view_mode()
 
 func toggle_view_mode():
-	freeze_all_inputs()
 	in_3d_mode = !in_3d_mode
 
 	if in_3d_mode:
@@ -44,8 +31,9 @@ func toggle_view_mode():
 func exit_2d():
 	player2d.set_process(false)
 	player2d.set_physics_process(false)
-	fadeinout.play("fade_out")
-	await get_tree().create_timer(1.5).timeout
+	player2d.set_process_input(false)
+	fadecontroller.play("fade_out")
+	await get_tree().create_timer(1.2).timeout
 	main_2d.visible = false
 	enter_3d()
 	
@@ -53,30 +41,35 @@ func exit_3d():
 	player3d.mouse_look_enabled = false
 	player3d.set_process(false)
 	player3d.set_physics_process(false)
-	fadeinout.play("fade_out")
-	await get_tree().create_timer(1.5).timeout
+	player3d.set_process_input(false)
+	fadecontroller.play("fade_out")
+	await get_tree().create_timer(1.2).timeout
+	player3d.queue_free()
 	main_3d.visible = false
 	enter_2d()
 	
 func enter_2d():
 	main_2d.visible = true
-	fadeinout.play("fade_in")
+	fadecontroller.play("fade_in")
 	await get_tree().create_timer(1).timeout
 	player2d.set_process(true)
 	player2d.set_physics_process(true)
 	player2d.set_process_input(true)
-	set_process_input(true)
 	
 func enter_3d():
 	main_3d.visible = true
+	player3d = player3dtemplate.instantiate()
+	main_3d.add_child(player3d)
+	player3danimator = player3d.get_node("Player3DAnimator")
 	player3d.position = Vector3(0, 0, 0.4)
-	player3d.camera_pivot.rotation = Vector2(0,0)
-	player3d.camera_pivot.Camera3D = Vector2(0,0)
-	fadeinout.play("fade_in")
-	periscopeinout.play("periscope_out")
+	player3d.set_process(false)
+	player3d.set_physics_process(false)
+	player3d.set_process_input(false)
+	player3d.mouse_look_enabled = false
+	fadecontroller.play("fade_in")
+	player3danimator.play("periscope_out")
 	await get_tree().create_timer(2).timeout
 	player3d.set_process(true)
 	player3d.set_physics_process(true)
 	player3d.set_process_input(true)
 	player3d.mouse_look_enabled = true
-	set_process_input(true)
